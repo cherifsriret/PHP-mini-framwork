9db5fdbf61dffcce0a4298e8c9abfa812406886d
cherif <cherifsriret@gmail.com>
2023-12-29 15:37:09 +0100

    Add database configuration

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -2,7 +2,9 @@
 
     require 'core/Router.php';
     require 'core/Request.php';
+    require 'core/database/Connection.php';
     require 'core/App.php';
     require 'helpers/Helper.php';
 
     App::load_config("config.php");
+    App::set('dbh', Connection::make(App::get('config')['database']));
---
+++ core/database/Connection.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * 
+ */
+
+class Connection 
+{
+    public static function make($config) 
+    {
+
+        try {
+            
+            $pdo = new PDO(
+                $config['hostname'].';port='.$config['port'].';dbname='.$config['dbname'],
+                $config['username'],
+                $config['password'],
+                $config['options']
+            );
+            return $pdo;
+            
+        } catch (PDOException $e) {
+            print_r($e);
+            die('Could not connect');
+        }
+        
+    }
+}
